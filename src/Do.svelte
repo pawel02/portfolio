6e603fe305b20f0e6f319e783f78f7f2ef4b4b20
u2090222 <pawel.bes@warwick.ac.uk>
2021-10-27 10:57:40 +0100

    added extra cards to the Do section

--- src/Do.svelte
+++ src/Do.svelte
@@ -14,11 +14,14 @@ Displays what I currently do and can do:
 
     function checkPosition()
     {
-        // get the position of #WhatImDoing
-        var rect = document.querySelectorAll(".section-wrapper")[0].getBoundingClientRect();
+        var rect1 = document.querySelectorAll(".section-wrapper")[0].getBoundingClientRect();
+        scrolledInViews[0] = scrolledInViews[0] ? true : rect1.y <= window.innerHeight - (rect1.height / 4);
 
-        scrolledInViews[0] = scrolledInViews[0] ? true : rect.y <= window.innerHeight - (rect.height / 4);
-        console.log(scrolledInViews[0]);
+        var rect2 = document.querySelectorAll(".section-wrapper")[1].getBoundingClientRect();
+        scrolledInViews[1] = scrolledInViews[1] ? true : rect2.y <= window.innerHeight - (rect2.height / 4);
+
+        var rect3 = document.querySelectorAll(".section-wrapper")[2].getBoundingClientRect();
+        scrolledInViews[2] = scrolledInViews[2] ? true : rect3.y <= window.innerHeight - (rect3.height / 4);
     }
 
     onMount(() => {
@@ -32,11 +35,39 @@ Displays what I currently do and can do:
 <section id = "WhatImDoing">
     <h2>What I do</h2>
 
-    <div class = "section-wrapper">
+    <div class = "section-wrapper section-wrapper-left">
         {#if scrolledInViews[0]}
         <div class = "card-wrapper" transition:fly="{{ y: 200, duration: 2000 }}">
-            <div class = "card">
+            <div class = "card card-left">
                 <h3>Contracting</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut illo blanditiis asperiores ullam exercitationem, corrupti labore at consequuntur voluptates, incidunt, iusto quibusdam accusamus aut hic fuga impedit autem tenetur voluptate.</p>
+            </div>
+        </div
+        ><div class = "image-wrapper center" transition:fly="{{ y: 150, duration: 1000 }}">
+            <img src = "./assets/contracting.jpg" alt = "Contracting">
+        </div>
+        {/if}
+    </div>
+
+    <div class = "section-wrapper section-wrapper-right">
+        {#if scrolledInViews[1]}
+        <div class = "image-wrapper center" transition:fly="{{ y: 150, duration: 1000 }}">
+            <img src = "./assets/contracting.jpg" alt = "Contracting">
+        </div
+        ><div class = "card-wrapper" transition:fly="{{ y: 200, duration: 2000 }}">
+            <div class = "card card-right">
+                <h3>Youtube</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut illo blanditiis asperiores ullam exercitationem, corrupti labore at consequuntur voluptates, incidunt, iusto quibusdam accusamus aut hic fuga impedit autem tenetur voluptate.</p>
+            </div>
+        </div>
+        {/if}
+    </div>
+
+    <div class = "section-wrapper section-wrapper-left">
+        {#if scrolledInViews[2]}
+        <div class = "card-wrapper" transition:fly="{{ y: 200, duration: 2000 }}">
+            <div class = "card card-left">
+                <h3>Software Engineering Apprenticeship</h3>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut illo blanditiis asperiores ullam exercitationem, corrupti labore at consequuntur voluptates, incidunt, iusto quibusdam accusamus aut hic fuga impedit autem tenetur voluptate.</p>
             </div>
         </div
@@ -66,10 +97,18 @@ h3
     height:400px;
     overflow:hidden;
     display:grid;
-    grid-template-columns: 1fr 1.5fr;
     align-items: center;
     justify-content: center;
     width:100%;
+}
+
+.section-wrapper-left
+{
+    grid-template-columns: 1fr 1.5fr;
+}
+.section-wrapper-right
+{
+    grid-template-columns: 1.5fr 1fr;
 }
 
 .card-wrapper
@@ -81,12 +120,20 @@ h3
 .card
 {
     width:90%;
-    margin-left:40%;
     border-radius:15px;
     box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
     padding:10px;
     background-color:#fff;
     border:1px solid var(--atomic-tangerine);
+}
+
+.card-left
+{
+    margin-left:40%;
+}
+.card-right
+{
+    margin-left:-40%;
 }
 
 p
